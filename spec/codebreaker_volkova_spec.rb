d5769324eaa9111cb8e428d10688daf65e4d1310
AnastasiusW <repositoryanastasiia@gmail.com>
2019-12-17 13:55:45 +0200

    Removed test dummy

--- spec/codebreaker_volkova_spec.rb
+++ spec/codebreaker_volkova_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe CodebreakerVolkova do
   it 'has a version number' do
     expect(CodebreakerVolkova::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
